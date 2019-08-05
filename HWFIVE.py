c31c6445fc916f62a5ba725c39023494dd07a814
Abrahamsquare <53538773+Abrahamsquare@users.noreply.github.com>
2019-08-05 06:46:21 -0700

    Add files via upload

---
+++ HWFIVE.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""
+Created on Tue Jul 28 19:44:04 2019
+
+@author: Abraham G Abraham
+Class:Foundations Of Programming: Python
+Assignment 5
+HW5.py
+Dependencies : Python 3.7
+
+
+Question
+
+How Can I break my script if the user choices other than option 1-5 example 6,7,8,9
+
+"""
+
+
+infile = "C:\Foundations Of Programming Python\ToDo.txt"
+
+# read in ToDo.txt
+with open(infile, 'r') as todo_file:
+    lines = todo_file.readlines()
+  
+task_dict = {}
+    
+for line in lines:
+   task = line.split(",")[0].strip()
+   priority = line.split(",")[1].strip()
+   task_dict[task] = priority
+   
+
+while(True):
+    print ("""
+    Menu of Options
+    1) Show current data
+    2) Add a new item.
+    3) Remove an existing item.
+    4) Save Data to File
+    5) Exit Program
+    """)
+    strChoice = str(input("Which option would you like to perform? [1 to 5] - "))
+    print()#adding a new line
+
+    # Choice 1 -Show the current items in the table
+    if (strChoice.strip() == '1'):
+        # loop through the dictionary here and print items
+        if (strChoice.strip() == '1'):
+            for key,value in task_dict.items():
+                print(key,value)
+    
+    # Choice 2 - Add a new item to the list/Table
+    elif(strChoice.strip() == '2'):
+        task_new = input("What is the name of the task?: ")
+        priority_new = input("Enter the new task's priority from low to high: ") 
+        task_dict[task_new] = priority_new
+           
+   
+    # Choice 3 - Remove a new item to the list/Table
+    elif(strChoice == '3'):
+        remove_key = input("Enter the task name to remove: ")
+        if remove_key in task_dict.keys():
+            del task_dict[remove_key]
+        else:
+            print(remove_key,"doesn't exist in the dictionary")
+   
+    # Choice 4 - Save tasks to the ToDo.txt file
+    elif(strChoice == '4'):
+        # open a file handle
+        user_save = input("Would you like to save your info? Type yes or no:")
+         # loop through key, value and write to file
+        if user_save.lower() == "yes":
+            with open(infile,"w") as op:
+                for key,value in task_dict.items():
+                    outline = "{},{}\n".format(key,value)
+                    op.write(outline)
+        
+        # Chocie 5- end the program1
+        elif (strChoice == '5'):break
+
+
+
+    
+
